003655b7fcf0b295a67c4a19fa06a9a16296b78b
Hemant Sharma <hs8112002@gmail.com>
2024-04-17 20:11:32 +0530

    Update settings.py

--- Blood_Bank/settings.py
+++ Blood_Bank/settings.py
@@ -63,16 +63,17 @@ WSGI_APPLICATION = 'Blood_Bank.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default':  dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+if not DEBUG:
+    DATABASES = {
+        'default':  dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
